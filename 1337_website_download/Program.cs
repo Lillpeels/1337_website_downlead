8747d7526dab95ebe6c37d363a569327fe052884
Fredrik <h18fresa@du.se>
2022-07-04 01:52:31 +0200

    a total remake of the program

--- 1337_website_download/Program.cs
+++ 1337_website_download/Program.cs
@@ -1,58 +1,123 @@
 ﻿using System;
+using System.IO;
 using System.Net;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+using System.Threading;
 
 namespace _1337_website_download
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Boolean programRunning = true;
+            // gets the path to the map user wanna save in
+            Console.WriteLine("Type save file path: ");
+            string saveFilePath = Console.ReadLine();
 
-            while (programRunning) {
+            // counter for the loading
+            int loading = 0;
 
-                Console.WriteLine("Type URL address: ");
-                String url = Console.ReadLine();
 
-                Console.WriteLine("Type save file path: ");
-                String sfp = Console.ReadLine();
-                
-                try 
-                { 
-                    DownloadFile(url, sfp);
-                }
-                catch(Exception e)
+            using (var client = new WebClient())
+            {
+                // gets the html from tretton37
+                var htmlSource = client.DownloadString("https://tretton37.com");
+
+                // loops through all the href links
+                foreach (var item in GetLinksFromWebsite(htmlSource))
                 {
-                    Console.WriteLine(e);
+                    // checks that the link is a link to a webpage and not anything else
+                    if (item.StartsWith("/"))
+                    {
+                        
+                        Webpage webPage = new Webpage();
+                        
+                        webPage.url = item;
+
+
+
+                        webPage.url = item;
+
+                        // creates the url of the subwebpages and downloads them
+                        Uri uri = new Uri("https://tretton37.com" + item);
+                        webPage.content = client.DownloadString(uri);
+
+                        // change the / to a - so it can be saved as the file name
+                        if (webPage.url.Contains("/"))
+                        {
+                            webPage.url = webPage.url.Replace("/", "-");
+                        }
+
+                        // create a thread for the creation of the file
+                        Thread thread1 = new Thread(() => createTxtFile(saveFilePath, webPage.url, webPage.content));
+                        thread1.Start();
+
+                        // the loading counter
+                        loading++;
+                        Console.Write("Loading: " + loading);
+
+                        Console.SetCursorPosition(0, Console.CursorTop);
+
+
+
+                    }
+
+
+
                 }
-                
+
+
+                Console.WriteLine("Download completed!");
+            }
+
+        }
+
+
+
+        public static List<String> GetLinksFromWebsite(string htmlSource)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlSource);
+            // gets the href links on the page
+            return doc
+                .DocumentNode
+                .SelectNodes("//a[@href]")
+                .Select(node => node.Attributes["href"].Value)
+                .ToList();
+
+            
+        }
+
+        public static void createTxtFile(string saveFilePath, string url, string content) {
+            string fileName = saveFilePath + @"\" + url + ".txt"; 
+            FileInfo fi = new FileInfo(fileName);
+
+            try
+            {
+                // Check if file already exists. If yes, delete it.     
+                if (fi.Exists)
+                {
+                    fi.Delete();
+                }
+
+                // Create a new file     
+                using (StreamWriter sw = fi.CreateText())
+                {
+                    sw.WriteLine(content);
+                    
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
             }
         }
-        
-        
-        public static void DownloadFile(string remoteFilename, string localFilename)
-        {
-            
-            WebClient client = new WebClient();
-            //client.DownloadFile(remoteFilename, localFilename);
-            Uri uri = new Uri(remoteFilename);
-            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressCallback);
-
-            client.DownloadFileAsync(uri, localFilename);
 
 
-        }
-
-        private static void DownloadProgressCallback(object sender, DownloadProgressChangedEventArgs e)
-        {
-            // Displays the operation identifier, and the transfer progress.
-            Console.WriteLine("{0}    downloaded {1} of {2} bytes. {3} % complete...",
-                (string)e.UserState,
-                e.BytesReceived,
-                e.TotalBytesToReceive,
-                e.ProgressPercentage);
-        }
 
     }
+    
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 1337_website_downlead
 The program is built in Visual Studio 2019
-You enter the URL to the website you want to download first
-after that you enter the path to the file you want to save it in, example a txt file.
-The webiste will then be saved there. 
+The program uses the nuget HtmlAgilityPack
+You enter the path to the map where you want to save the website, it will then create textfiles in it.
+
